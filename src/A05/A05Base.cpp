47467a4e98a512600f712a3ad4287411526ca85a
david lopez saludes <davidlopez1972@hotmail.es>
2017-10-17 15:45:53 +0200

    EVERYTHING IS HERE

---
+++ src/A05/A05Base.cpp
@@ -0,0 +1,92 @@
+#include <SDL.h>		// Always needs to be included for an SDL app
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+//Game general information
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(int, char*[]) {
+
+	// --- INIT ---
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
+
+	const Uint8 imgFlags{ IMG_INIT_PNG | IMG_INIT_JPG };
+	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error : SDL_image init";
+
+	if (TTF_Init() != 0) "No es pot inicialitzar SDL_ttf";
+
+	// --- WINDOW ---
+	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
+	if (window == nullptr) throw "No es pot inicialitzar SDL_Window";
+
+	// --- RENDERER ---
+	SDL_Renderer *renderer{ SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
+	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
+
+	// --- SPRITES ---
+	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../res/img/bg.jpg") };
+	if (bgTexture == nullptr) throw "No s'ha pogut crear les textures";
+	SDL_Rect bgRect{ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+
+	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../res/img/Kintoun.png") };
+	if(playerTexture == nullptr) throw "No s'ha pogut crear les textures";
+	SDL_Rect playerRect{ 0, 0, 350, 189 };
+	SDL_Rect playerTarget{ 0, 0, 100, 100 };
+		// --- Animated Sprite ---
+	
+	// --- TEXT ---
+	TTF_Font *font{ TTF_OpenFont("../res/ttf/saiyan.ttf", 80) };
+	if (font == nullptr) throw "No es pot inicialitzar the TTF_Font";
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "My first SDL game", SDL_Color{255, 150, 0, 255}) };
+	if (tmpSurf == nullptr) throw "UNable to exist XD jajajaja LOL";
+	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(renderer, tmpSurf) };
+	SDL_Rect textRect{ 100, 50, tmpSurf->w, tmpSurf->h };
+	SDL_FreeSurface(tmpSurf);
+	TTF_CloseFont(font);
+	// --- AUDIO ---
+
+	// --- GAME LOOP ---
+	SDL_Event event;
+	bool isRunning = true;
+	while (isRunning){
+		// HANDLE EVENTS
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:		isRunning = false; break;
+			case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
+			case SDL_MOUSEMOTION:
+				playerTarget.x = event.motion.x - 50; 
+				playerTarget.y = event.motion.y - 50;
+				break;
+			default:;
+			}
+		}
+
+		// UPDATE
+		playerRect.x += (playerTarget.x - playerRect.x) / 10;
+		playerRect.y += (playerTarget.y - playerRect.y) / 10;
+		// DRAW
+			//Background
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
+		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+		SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
+			//Animated Sprite
+		SDL_RenderPresent(renderer);
+
+	}
+
+	// --- DESTROY ---
+	SDL_DestroyTexture(bgTexture);
+	SDL_DestroyTexture(playerTexture);
+	SDL_DestroyTexture(textTexture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	// --- QUIT ---
+	IMG_Quit();
+	TTF_Quit();
+	SDL_Quit();
+	return 0;
+}
